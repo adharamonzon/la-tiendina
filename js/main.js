81ef451542ec8a20538b87c892fd5baf0975bb1b
adharamonzon <adharamonzon@gmail.com>
2021-03-24 17:53:24 +0100

    painted groceries from .json, add initial styles to the grocerie list

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
-const vegetables = [];
+let vegetables = [];
 
 const getData = () => {
  return fetch('../js/data/data.json')
 .then(response => response.json())
-.then(data => {return vegetables.push(data)
+.then(data => {
+   vegetables = data;
+   paintGroceries();
   })
 
 }
 
 console.log(vegetables)
-
 const vegetablesList = document.querySelector('.js-vegetables')
 
-const paintVegetables = () => {
+const paintGroceries = () => {
  let listCode = ''; 
  for (vegetable of vegetables) {
-   console.log(vegetable.name)
+  console.log(vegetable.name);
    if (vegetable.family === "verduras"){
-   listCode += `<li><h4>${vegetable.name}</h4>`;
-   listCode += `<img src=${vegetable.url} />}`;
-   listCode += `</li>`
- }
+   listCode += `<li class="vegetables-item">`;
+   listCode += '<div class="image-content">'
+   listCode += `<img class="vegetables-img" src=${vegetable.url} />`;
+   listCode += '</div>'
+   listCode += `<h6 class="vegetables-title">${vegetable.name}</h6>`;
+   listCode += `<p class="vegetables-price">${vegetable.price}€ /Kg </p>`;
+   listCode += `</li>`;
+ } 
 }
 vegetablesList.innerHTML = listCode;
 }
 
 getData();
-paintVegetables()
